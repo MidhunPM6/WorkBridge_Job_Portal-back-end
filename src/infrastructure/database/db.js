434969fe92ea393392faa1bd3e65455e9cdf55a6
Midhunpm6 <midhunpm6060@gmail.com>
2025-05-18 23:01:24 +0530

    refactor: Changed the MongoDB compass to atlas

--- src/infrastructure/database/db.js
+++ src/infrastructure/database/db.js
@@ -1,12 +1,23 @@
+// src/config/db.js
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const   connectDB = () =>{
-    mongoose.connect('mongodb://localhost:27017/',{
-        dbName : 'workBridge'
-    }).then(()=>{
-        console.log("MongoDB connected");
-    })
-}
+dotenv.config(); 
 
-export default connectDB()
- 
+const mongoUsername = process.env.MONGO_USERNAME;
+const mongoPassword = encodeURIComponent(process.env.MONGO_PASSWORD); 
+
+const connectDB = async () => {
+  try {
+    const uri = `mongodb+srv://${mongoUsername}:${mongoPassword}@cluster0.p2cn2fu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+    await mongoose.connect(uri, {
+      dbName: "workBridge",
+    });
+    console.log(" MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection failed:", error.message);
+    process.exit(1); 
+  }
+};
+
+export default connectDB();

c856cbe8ac0f4f69fc657996dfde600f955d8838
Midhunpm6 <midhunpm6060@gmail.com>
2025-05-22 12:42:44 +0530

    Fix: Error fix in crocs

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ app.use(cookieParser())
  
 
 app.use(cors({
-   origin : ["http://localhost:3000", "https://work-bridge-sooty.vercel.app/"],
+   origin : ["http://localhost:3000", "https://work-bridge-sooty.vercel.app"],
    methods: ["GET", "POST", "PUT", "DELETE"],
    credentials : true
 }))

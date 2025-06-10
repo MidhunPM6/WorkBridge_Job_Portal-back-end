16e4bd9a1dc34b80ce54dd31630a7279af31a796
Midhunpm6 <midhunpm6060@gmail.com>
2025-06-10 15:10:44 +0530

    refactor : some changes are made

--- src/interface-adapter/routes/employer/employerRoute.js
+++ src/interface-adapter/routes/employer/employerRoute.js
@@ -1,18 +1,53 @@
 import express from 'express'
-import { employerJobPostController,fetchMyJobsController ,updateJobController,deleteJobController,employerProfileController,fetchCompanyProfileController} from '../../controllers/employer/employerController.js'
+import {
+  employerJobPostController,
+  fetchMyJobsController,
+  updateJobController,
+  deleteJobController,
+  employerProfileController,
+  fetchCompanyProfileController
+} from '../../controllers/employer/employerController.js'
 import { verifyToken } from '../../../infrastructure/middleware/jwtVerifyMiddle.js'
 import { authorizeRoles } from '../../../infrastructure/middleware/authenticateUser.js'
-
 
 const router = express.Router()
 // Employer profile routes
 
-router.post('/postjob', verifyToken, authorizeRoles('employer'), employerJobPostController)
-router.get('/myjobs', verifyToken, authorizeRoles('employer'), fetchMyJobsController)
-router.patch('/updateJob/:id', verifyToken, authorizeRoles('employer'), updateJobController)
-router.delete('/deleteJob/:id', verifyToken, authorizeRoles('employer'), deleteJobController)
-router.post('/profile',verifyToken,authorizeRoles('employer'),employerProfileController)
-router.get('/profile',verifyToken,authorizeRoles('employer'),fetchCompanyProfileController)
-
+router.post(
+  '/postjob',
+  verifyToken,
+  authorizeRoles('employer'),
+  employerJobPostController
+)
+router.get(
+  '/myjobs',
+  verifyToken,
+  authorizeRoles('employer'),
+  fetchMyJobsController
+)
+router.patch(
+  '/updateJob/:id',
+  verifyToken,
+  authorizeRoles('employer'),
+  updateJobController
+)
+router.delete(
+  '/deleteJob/:id',
+  verifyToken,
+  authorizeRoles('employer'),
+  deleteJobController
+)
+router.post(
+  '/profile',
+  verifyToken,
+  authorizeRoles('employer'),
+  employerProfileController
+)
+router.get(
+  '/profileData',
+  verifyToken,
+  authorizeRoles('employer'),
+  fetchCompanyProfileController
+)
 
 export default router
